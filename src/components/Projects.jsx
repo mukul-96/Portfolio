1333660f0c6aa9a48ef343575d3c2188123ce26b
Mukul <aggarwalmukul96@gmail.com>
2024-05-26 00:46:11 +0530

    project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ const Projects=()=>{
     
     <div className="sm:text-[18px] text-[16px] text-taupe uppercase tracking-wider font-semibold    font-poppins text-gray-400 ">CASE STUDIES</div>
         <h1 className="text-timberWolf font-black md:text-[60px] sm:text-[48px] xs:text-[40px] text-[30px] font-poppins text-white">Projects.</h1>
+        <div className="text-gray-300 mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">These projects demonstrate my expertise with practical examples of some of my work, including brief descriptions and links to code repositories and live demos. They showcase my ability to tackle intricate challenges, adapt to various technologies, and efficiently oversee projects.</div>
     </div>
         </div>
     )
